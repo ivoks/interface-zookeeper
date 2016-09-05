28a1af5227e012862f6a45cadc96d983ac9f0f48
Ante Karamatic <ante.karamatic@canonical.com>
2016-09-05 16:41:43 +0200

    Make sure to expose host if set, instead of always using private-address

--- requires.py
+++ requires.py
@@ -42,9 +42,12 @@ class ZookeeperRequires(RelationBase):
         for conv in self.conversations():
             port = conv.get_remote('port')
             rest_port = conv.get_remote('rest_port')
+            host = conv.get_remote('host')
+            if not host:
+                host = conv.get_remote('private-address')
             if port:
                 zks.append({
-                    'host': conv.get_remote('private-address'),
+                    'host': host,
                     'port': port,
                     'rest_port': rest_port
                 })
